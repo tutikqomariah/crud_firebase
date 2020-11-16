c4003aa3b0b7fc7f6cb15d0dbf7bbfbb10a8d851
tutikqomariah <71108601+tutikqomariah@users.noreply.github.com>
2020-11-16 22:30:11 +0700

    first commit

---
+++ app/src/main/java/com/tutik/crud_firebase/LoginActivity.kt
@@ -0,0 +1,76 @@
+package com.tutik.crud_firebase
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var auth: FirebaseAuth? = null
+    private val RC_SIGN_IN = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val progress = findViewById<ProgressBar>(R.id.progress)
+        progress.visibility = View.GONE
+        val login = findViewById<Button>(R.id.login)
+        login.setOnClickListener(this)
+        auth = FirebaseAuth.getInstance() //Mendapakan Instance Firebase Auth
+
+        //Cek apakah sudah login atau belum
+        if (auth!!.currentUser == null) {
+        } else {
+            //Jika sudah login langsung dilempar ke MainActivity
+            intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    override fun onClick(p0: View?) {
+        // Choose authentication providers
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.PhoneBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build())
+
+        // Create and launch sign-in intent
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                val user = FirebaseAuth.getInstance().currentUser
+                Toast.makeText(this, "Login Berhasil", Toast.LENGTH_SHORT).show()
+                intent = Intent(applicationContext, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+
+            } else {
+                Toast.makeText(this, "Login Dibatalkan", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
+}
